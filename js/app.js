dc920d99cdf41b1a0ac0b62e1829a1190464cdcd
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-17 14:46:24 +0200

    add filter logic

--- js/app.js
+++ js/app.js
@@ -25,3 +25,16 @@ iconTypes.forEach((type) => {
   const typeCapitalized = type.charAt(0).toUpperCase() + type.slice(1);
   selectElm.innerHTML += `<option value="${type}">${typeCapitalized}</option>`;
 });
+
+selectElm.addEventListener("change", function (e) {
+  const type = e.target.value;
+  const boxes = document.querySelectorAll(".box");
+
+  boxes.forEach((box) => {
+    if (box.classList.contains(`box--${type}`) || type === "all") {
+      box.classList.remove("hidden");
+    } else {
+      box.classList.add("hidden");
+    }
+  });
+});
